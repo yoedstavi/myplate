yoedstavi/myplate

commit 5d4e2d449a240998528c8c4efdc45409a3b1fe9a

Larger default font size in the editor

src/renderer.ts
@@ -389,6 +389,7 @@ function initTaskElements(
     maxLines: 40,
     minLines: 10,
     printMargin: false,
+    fontSize: 18
   });
   aceEditor.session.on('change', event => taskObj.notes = aceEditor.getValue());
   aceEditor.commands.addCommand({

commit a96c8daa76bcc65d21c9eb44bfdbb60e56c07772

bugfix - fix blocked-by functionality
broken by:
5d4e2d449a240998528c8c4efdc45409a3b1fe9a

src/renderer.ts
@@ -693,6 +693,7 @@ function fillBlockedByTasks(uuid: string, title: string, dependencyList: string[
   h3title.innerText = title;
   blockedByTasksList.replaceChildren(h3title);
 
+  const taskObjBlocked = resolveTaskByUuid(uuid);
   for (const [taskId, taskObj] of Object.entries(taskData)) {
     // prevent circular dependency
     if (taskId == uuid || taskObj.isDone || allBlocking.has(taskId))
@@ -702,12 +703,12 @@ function fillBlockedByTasks(uuid: string, title: string, dependencyList: string[
 
     const checkFunc = (chk: boolean) => {
       if (chk) {
-        addTaskToDependencyList(taskObj, taskId);
-        evaluateDoneButtonState(uuid, taskObj);
+        addTaskToDependencyList(taskObjBlocked, taskId);
+        evaluateDoneButtonState(uuid, taskObjBlocked);
       }
       else {
-        removeTaskFromDependencyList(taskObj, taskId);
-        evaluateDoneButtonState(uuid, taskObj);
+        removeTaskFromDependencyList(taskObjBlocked, taskId);
+        evaluateDoneButtonState(uuid, taskObjBlocked);
       }
     };
 
@@ -733,7 +734,6 @@ function fillEditCategory(category: string) {
     if (taskObj.isDone) // skip done tasks
       continue;
 
-    const ctrl = crypto.randomUUID();
     const inCategory = (taskObj.categories.lastIndexOf(category) != -1);
 
     const checkFunc = (chk: boolean) => {